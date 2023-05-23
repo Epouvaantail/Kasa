7bea222080eb39e0e18d67134db4bb14dde4b09c
Epouvaantail <coutouzis.kilian@gmail.com>
2023-05-23 15:31:18 +0200

    update

---
+++ kasa/src/pages/Logement.js
@@ -0,0 +1,20 @@
+import * as React from 'react';
+import { Routes, Route, useParams } from 'react-router-dom';
+import { useNavigate } from "react-router-dom";
+
+function ProfilePage() {
+  // Get the userId param from the URL.
+  let { userId } = useParams();
+  // ...
+}
+
+function App() {
+  return (
+    <Routes>
+      <Route path="users">
+        <Route path=":userId" element={<ProfilePage />} />
+        <Route path="me" element={...} />
+      </Route>
+    </Routes>
+  );
+}
