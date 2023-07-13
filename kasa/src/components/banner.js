0f9517f5d1a2a42a199d3b26451c6c7444dd9c85
Epouvaantail <coutouzis.kilian@gmail.com>
2023-07-13 08:52:25 +0200

    code upgrade

--- kasa/src/components/banner.js
+++ kasa/src/components/banner.js
@@ -1,22 +1,15 @@
 import React from "react";
 import "../style/main.css";
 import Cliff from "../assets/images/falaise.png";
-import { useLocation } from "react-router-dom";
 
 const Banner = () => {
-  const location = useLocation();
   return (
-    <div
-      className="home__banner"
-      style={location.pathname === "/a-propos" ? { height: "211px" } : {}}
-    >
-      <img src={Cliff} alt="banner kasa" />
-      {(location.pathname === "/" || location.pathname === "/Kasa") && (
-        <h1>
-          Chez vous,
-          <span className="line-break"></span> partout et ailleurs
-        </h1>
-      )}
+    <div className="home__banner">
+      <img src={Cliff} alt="banner kasa" />      
+      <h1>
+        Chez vous,
+        <span className="line-break"></span> partout et ailleurs
+      </h1>
     </div>
   );
 };
